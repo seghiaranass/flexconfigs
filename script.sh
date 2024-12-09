4d64c42cff8911b33ada5daf81981f0790d51bc3
seghiaranass <anass.segh97@gmail.com>
2024-12-09 11:06:20 +0100

    Local laravel 11 installation with docker compose has completed

--- script.sh
+++ script.sh
@@ -17,15 +17,15 @@ cd /root/.a/workspaces/flex-configs/
 
 # Build and start necessary services
 docker compose run --rm --remove-orphans composer 
-docker compose up -d php server --build --remove-orphans
+docker compose up -d mysql php server --build --remove-orphans
 
 # Wait for MySQL to be ready (max 30s)
-# echo "Waiting for MySQL to be ready..."
-# until docker compose exec mysql mysqladmin ping -h"mysql" --silent; do
-#   echo "Waiting for MySQL..."
-#   sleep 2
-# done
-# echo "MySQL is ready!"
+echo "Waiting for MySQL to be ready..."
+until docker compose exec mysql mysqladmin ping -h"mysql" --silent; do
+  echo "Waiting for MySQL..."
+  sleep 2
+done
+echo "MySQL is ready!"
 
 # Set proper permissions for storage
 sudo chown -R www-data:www-data ./code/storage/
