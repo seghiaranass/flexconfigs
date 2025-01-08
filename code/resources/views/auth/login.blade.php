bb5ff55cfb25dd80b056bf4dfcc4b05ef4849f41
seghiaranass <anass.segh97@gmail.com>
2025-01-08 23:03:11 +0100

    a lot of files

--- code/resources/views/auth/login.blade.php
+++ code/resources/views/auth/login.blade.php
@@ -1,42 +1,32 @@
-@extends('layouts.guest')
+<x-base-layout :scrollspy="false">
 
-@section('title', 'Login')
+    <x-slot:pageTitle>
+        {{-- {{$title}}  --}}
+    </x-slot>
 
-@section('content')
-<div class="form">
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <x-slot:headerFiles>
+        <!--  BEGIN CUSTOM STYLE FILE  -->
+        @vite(['resources/scss/light/assets/authentication/auth-boxed.scss'])
+        @vite(['resources/scss/dark/assets/authentication/auth-boxed.scss'])
 
-  <!-- BEGIN LOADER -->
-  <div id="load_screen"> <div class="loader"> <div class="loader-content">
-        <div class="spinner-grow align-self-center"></div>
-    </div></div></div>
-    <!--  END LOADER -->
-
+        <style>
+            #load_screen {
+                display: none;
+            }
+        </style>
+        <!--  END CUSTOM STYLE FILE  -->
+    </x-slot>
+    <!-- END GLOBAL MANDATORY STYLES -->
+    
     <div class="auth-container d-flex">
 
         <div class="container mx-auto align-self-center">
     
             <div class="row">
     
-                <div class="col-6 d-lg-flex d-none h-100 my-auto top-0 start-0 text-center justify-content-center flex-column">
-                    <div class="auth-cover-bg-image"></div>
-                    <div class="auth-overlay"></div>
-                        
-                    <div class="auth-cover">
-    
-                        <div class="position-relative">
-    
-                            <img src="../src/assets/img/auth-cover.svg" alt="auth-img">
-    
-                            <h2 class="mt-5 text-white font-weight-bolder px-2">Join the community of expert developers</h2>
-                            <p class="text-white px-2">It is easy to setup with great customer experience. Start your 7-day free trial</p>
-                        </div>
-                        
-                    </div>
-
-                </div>
-
-                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center ms-lg-auto me-lg-0 mx-auto">
-                    <div class="card">
+                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
+                    <div class="card mt-3 mb-3">
                         <div class="card-body">
     
                             <div class="row">
@@ -87,7 +77,7 @@
                                 <div class="col-sm-4 col-12">
                                     <div class="mb-4">
                                         <button class="btn  btn-social-login w-100 ">
-                                            <img src="../src/assets/img/google-gmail.svg" alt="" class="img-fluid">
+                                            <img src="{{Vite::asset('resources/images/google-gmail.svg')}}" alt="" class="img-fluid">
                                             <span class="btn-text-inner">Google</span>
                                         </button>
                                     </div>
@@ -96,7 +86,7 @@
                                 <div class="col-sm-4 col-12">
                                     <div class="mb-4">
                                         <button class="btn  btn-social-login w-100">
-                                            <img src="../src/assets/img/github-icon.svg" alt="" class="img-fluid">
+                                            <img src="{{Vite::asset('resources/images/github-icon.svg')}}" alt="" class="img-fluid">
                                             <span class="btn-text-inner">Github</span>
                                         </button>
                                     </div>
@@ -105,7 +95,7 @@
                                 <div class="col-sm-4 col-12">
                                     <div class="mb-4">
                                         <button class="btn  btn-social-login w-100">
-                                            <img src="../src/assets/img/twitter.svg" alt="" class="img-fluid">
+                                            <img src="{{Vite::asset('resources/images/twitter.svg')}}" alt="" class="img-fluid">
                                             <span class="btn-text-inner">Twitter</span>
                                         </button>
                                     </div>
@@ -129,8 +119,9 @@
 
     </div>
     
--->
+    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+    <x-slot:footerFiles>
 
-
-</div>
-@endsection
+    </x-slot>
+    <!--  END CUSTOM SCRIPTS FILE  -->
+</x-base-layout>
