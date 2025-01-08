208b9a062f06c9542a3bcd70b3ee7a84d6511121
Anass Seghiar <anass.segh97@gmail.com>
2025-01-08 11:03:08 +0100

    All things ready now

--- code/resources/views/layouts/guest.blade.php
+++ code/resources/views/layouts/guest.blade.php
@@ -2,9 +2,10 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+       
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
